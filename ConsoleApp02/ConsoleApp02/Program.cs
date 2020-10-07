41ab51c459b75304b65bf284ee0c97678c6de628
john cravener <johncrav@microsoft.com>
2020-10-06 23:56:52 -0700

    sorting an object

---
+++ ConsoleApp02/ConsoleApp02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            //int rval = rnd.Next(1,6);
+            string[] names = new string[8] { "john", "beth", "london", "jane", "windsor", "elise", "ann", "harold" };
+
+            myObj[] mos = new myObj[names.Length]; 
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                mos[i] = new myObj(Convert.ToString(rnd.Next(1, names.Length)), util.CapFirstLetter(names[i]));
+            }
+
+            foreach(var o in mos.OrderBy(x => x.dtstamp))
+            {
+                Console.WriteLine($"{o.dtstamp} {o.name}");
+            }
+        }
+    }
+
+    class myObj
+    {
+        private string dtSuffix = "2020-10-";
+        public DateTime dtstamp;
+        public string name;
+
+        public myObj(string s, string n)
+        {
+            this.dtstamp = DateTime.Parse(this.dtSuffix + s);
+            this.name = n;
+        }
+    }
+
+    static class util
+    {
+        public static string CapFirstLetter(string s)
+        {
+            if(s[0] >= 97 && s[0] <= 123)
+            {
+                s = Convert.ToChar(s[0] - 32) + s.Substring(1);                
+            }
+            return s;
+        }
+    }
+}
