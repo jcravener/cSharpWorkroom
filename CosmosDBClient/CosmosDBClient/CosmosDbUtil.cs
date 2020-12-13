f7b72d0b35be1de782e0d3d5a07c4506df9ab65a
john cravener <johncrav@microsoft.com>
2020-12-13 12:21:24 -0800

    initial checkin

---
+++ CosmosDBClient/CosmosDBClient/CosmosDbUtil.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDBClient
+{
+    class CosmosDbUtil
+    {
+        public CosmosConn ParseCosmosConnStr(string connStr)
+        {
+            CosmosConn returnVal = new CosmosConn();
+
+            foreach (string piece in connStr.Split(";"))
+            {
+                if (!string.IsNullOrEmpty(piece))
+                {
+                    if (piece.StartsWith("AccountKey="))
+                    {
+                        returnVal.AccountKey = piece.Substring("AccountKey=".Length);
+                    }
+                    if (piece.StartsWith("AccountEndpoint="))
+                    {
+                        returnVal.AccountEndpoint = piece.Substring("AccountEndpoint=".Length);
+                    }
+                }
+            }
+
+            return returnVal;
+        }
+
+        public async Task<List<DatabaseProperties>> ListDatabases(CosmosClient cosmosClient)
+        {
+            List<DatabaseProperties> databaseProperties = new List<DatabaseProperties>();
+
+            using(FeedIterator<DatabaseProperties> iterator = cosmosClient.GetDatabaseQueryIterator<DatabaseProperties>())
+            {
+                while (iterator.HasMoreResults)
+                {
+                    foreach( DatabaseProperties db in await iterator.ReadNextAsync())
+                    {
+                        databaseProperties.Add(db);
+                    }
+                }
+            }
+            return databaseProperties;
+        }
+
+        public async Task<List<ContainerProperties>> ListContainers(Database cosmosDb)
+        {
+            List<ContainerProperties> containerProperties = new List<ContainerProperties>();
+
+            using (FeedIterator<ContainerProperties> iterator = cosmosDb.GetContainerQueryIterator<ContainerProperties>())
+            {
+                while (iterator.HasMoreResults)
+                {
+                    foreach(ContainerProperties container in await iterator.ReadNextAsync())
+                    {
+                        containerProperties.Add(container);
+                    }
+                }
+            }
+            return containerProperties;
+        }
+    }
+
+    class CosmosConn
+    {
+        public string AccountKey { get; set; }
+        public string AccountEndpoint { get; set; }
+    }
+}
---
+++ CosmosDBClient/CosmosDBClient/Program.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+
+namespace CosmosDBClient
+{
+    class Program
+    {
+        public static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            string usgMsg = $"usage: {System.AppDomain.CurrentDomain.FriendlyName} <CosmosConnStr> [DatabaseName] [ContainerName] [Id] [PartitionKey]";
+            if (args.Length < 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Error.WriteLine(usgMsg);
+                Console.ForegroundColor = ConsoleColor.White;
+                Environment.Exit(0);
+            }
+            string connStr = args[0];
+
+            CosmosClient cosmosClient = null;
+            Database cosmosDatabase;
+            CosmosDbUtil cosmosDbUtil = new CosmosDbUtil();
+            CosmosConn cosmosConn;            
+            
+            try
+            {
+                cosmosConn = cosmosDbUtil.ParseCosmosConnStr(connStr);
+                cosmosClient = new CosmosClient(cosmosConn.AccountEndpoint, cosmosConn.AccountKey);
+                //cosmosDatabase = cosmosClient.GetDatabase(databaseName);
+            }
+            catch (CosmosException ce)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine($"Had problems creating CosmosDB client.");
+                Console.Error.WriteLine(ce.ToString());
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            List<DatabaseProperties> databaseProperties;
+            List<ContainerProperties> containerProperties;
+            if(args.Length == 1) // list databases
+            {
+                try
+                {
+                    databaseProperties = new List<DatabaseProperties>();
+
+                    databaseProperties = await cosmosDbUtil.ListDatabases(cosmosClient);
+                    Console.WriteLine("DatabaseId");
+                    foreach(DatabaseProperties dbp in databaseProperties)
+                    {
+                        Console.WriteLine(dbp.Id);
+                    }
+                }
+                catch (CosmosException ce)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine($"Had problems listing CosmosDB databases.");
+                    Console.Error.WriteLine(ce.ToString());
+                    Environment.Exit(0);
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+            }
+            else if(args.Length == 2) // list containers
+            {
+                try
+                {
+                    containerProperties = new List<ContainerProperties>();
+
+                    cosmosDatabase = cosmosClient.GetDatabase(args[1]);
+                    containerProperties = await cosmosDbUtil.ListContainers(cosmosDatabase);
+                    Console.WriteLine("ContainerId,PartitionKeyPath");
+                    foreach(ContainerProperties cp in containerProperties)
+                    {
+                        Console.WriteLine($"{cp.Id},{cp.PartitionKeyPath}");
+                    }
+                }
+                catch (CosmosException ce)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine($"Had problems listing CosmosDB containers in {args[1]}.");
+                    Console.Error.WriteLine(ce.ToString());
+                    Environment.Exit(0);
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Error.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+
+            }
+        }
+    }
+}
