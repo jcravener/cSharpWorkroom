2912346ba0b0a8f73cb1fcc78e29412531c225f9
jcravener <38707228+jcravener@users.noreply.github.com>
2021-10-03 20:16:28 -0700

    Initial checkin of ParseYaml.  It converts yaml file to json, and json files to yaml.

---
+++ Convert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using YamlDotNet.Serialization;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace ParseYaml
+{
+    class Convert
+    {
+        public static String ToJson(StringReader Reader)
+        {
+            var deserializer = new DeserializerBuilder().Build();
+            var yamlObject = deserializer.Deserialize(Reader);
+
+            var serializer = new SerializerBuilder()
+                .JsonCompatible()
+                .Build();
+
+            String json = serializer.Serialize(yamlObject);
+
+            return json;
+        }
+
+        public static String toYaml(String Txt)
+        {
+            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(Txt);
+
+            var serializer = new SerializerBuilder().Build();
+            var yaml = serializer.Serialize(obj);
+
+            return yaml;
+        }
+    }
+}
