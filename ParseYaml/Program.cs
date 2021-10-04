ee95bc710d7bc2165e51901710318cc0c9ebb5ea
john cravener <johncrav@microsoft.com>
2021-10-03 21:12:10 -0700

    Updated command line args processing logic

--- ParseYaml/Program.cs
+++ ParseYaml/Program.cs
@@ -10,21 +10,21 @@ namespace ParseYaml
         static void Main(string[] args)
         {
             String usage = $"usage: {System.AppDomain.CurrentDomain.FriendlyName} -y /path/to/yaml/file | -j /path/to/yaml/file";
-            Regex pat = new Regex(@"^\-[yj]");
-            Match m = pat.Match(args[0]);
 
-            if (args.Length.Equals(0))
+            if (args.Length < 2 )
             {
                 Console.Error.Write(usage);
                 Environment.Exit(1);
             }
 
+            Regex pat = new Regex(@"^\-[yj]");
+            Match m = pat.Match(args[0]);
+
             String txt;
             String output;
 
             if (m.Success)
-            {
-                
+            {                
                 try
                 {
                     txt = File.ReadAllText($@"{args[1]}");
@@ -52,11 +52,6 @@ namespace ParseYaml
                 Console.Error.Write(usage);
                 Environment.Exit(1);
             }
-
-
-
-
-
         }
     }
 }
