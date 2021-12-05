c1d9ebcb720792a438cb425d1b41ed45f30785ef
John Cravener <johncrav@microsoft.com>
2021-12-04 16:24:21 -0800

    Initia checkin of valid paren

---
+++ LeetCode/ValidParentheses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Input not provided.");
+                [System.Environment.Exit(1);
+            }
+
+            string s = args[0];
+        }
+
+        public static bool IsValid(string s)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>()
+            {
+                { "paren", 0},
+                { "square", 0},
+                { "curly", 0}
+            };
+
+
+
+            Regex parenRx = new Regex(@"[\(\)]", RegexOptions.Compiled);
+            Regex squareRx = new Regex(@"[\[\]]", RegexOptions.Compiled);
+            Regex curlyRx = new Regex(@"[\{\}]", RegexOptions.Compiled);
+
+            foreach(char c in s)
+            {
+                if( c.Equals('(') || c.Equals(')'))
+                {
+                    map["paren"] += 1
+                }
+            }
+
+            
+            return false;
+        }
+    }
+}
