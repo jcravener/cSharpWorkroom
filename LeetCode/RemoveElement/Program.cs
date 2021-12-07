c0884e36ba7b454dfcfedac1adb794fe479e7686
john cravener <johncrav@microsoft.com>
2021-12-06 20:15:06 -0800

    Checkin of: 27. Remove Element

---
+++ LeetCode/RemoveElement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3,2,2,3 };
+            int v = 3;
+            
+            Console.WriteLine(RemoveElement(a, v).ToString());
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {            
+            if (nums.Length == 0)
+            {
+                return nums.Length;
+            }
+
+            if(nums.Length == 1 && nums[0] == val)
+            {
+                nums[0] = val + 1;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = 101;
+                    count++;
+                }
+            }
+
+            Array.Sort(nums);
+
+            return nums.Length-count;
+        }
+    }
+}
