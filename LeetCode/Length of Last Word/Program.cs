88fac0c903601b0f83d861d9e4e955359ac95684
john cravener <jcravener@hotmail.com>
2022-07-03 12:04:12 -0700

    Leetcode prob 58

---
+++ LeetCode/Length of Last Word/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+string s = "   fly me   to   the moon  ";
+
+Solution Solution = new Solution();
+
+Console.WriteLine(Solution.LengthOfLastWord(s));
+
+public class Solution
+{   
+    public int LengthOfLastWord(string s)
+    {
+        if(s.Length == 0)
+        {
+            return 0;
+        }
+
+        string[] words = Regex.Split(s.Trim(), @"\s+");
+
+        return words[words.Length - 1].Length;
+    }
+}
