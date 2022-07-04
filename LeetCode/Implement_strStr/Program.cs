1817e80a10c3e95962aa50e70a9c16100bf2cc01
john cravener <jcravener@hotmail.com>
2022-07-04 10:50:13 -0700

    Adding comments to problem #28

--- LeetCode/Implement_strStr/Program.cs
+++ LeetCode/Implement_strStr/Program.cs
@@ -13,32 +13,28 @@ namespace Implement_strStr
             Console.WriteLine(StrStr(h, n).ToString());
         }
 
-        // This solution doesn't work.  
-
-        // Another approach would be to check substrings.
-        // 1) once you find maching first chars,
-        // 2) compare the substring in haystack starting at the first found index and ending with an offset of needle.Length
-        // 3) If they don't match, start over - but always check whether the length of the raminaing substring is >= needle
 
         public static int StrStr(string haystack, string needle)
-        {
+        {            
+            // If needle is emty, return 0 by definition.  Or if needle equals hasytack retun the first index
             if (string.IsNullOrEmpty(needle) || haystack.Equals(needle))
             {
                 return 0;
             }
 
+            // If needle is longer than haystack it cannot be a substring.
             if(needle.Length > haystack.Length)
             {
                 return -1;
             }
 
-            //char[] h = haystack.ToCharArray();
-            //char[] n = needle.ToCharArray();
-
+            // loop through each char of haystack looking for a match with the first char in needle            
             for(int i = 0; i < haystack.Length; i++)
             {
+                //  If we find a char match between the first needle char, check wether the substring of equal length to needle is a string match
                 if(haystack[i] == needle[0])
                 {
+                    //  Be sure we have the enough remaining hastack chars to equal the lengh of the needle
                     if (haystack.Length - i >= needle.Length)
                     {
                         if (needle.Equals(haystack.Substring(i, needle.Length)))
@@ -46,7 +42,7 @@ namespace Implement_strStr
                             return i;
                         }
                     }
-                    else
+                    else // we dont have enough chars in the hasystach substring length so there is no way to match
                     {
                         break;
                     }
