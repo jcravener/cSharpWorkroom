4d95f2c1f4431f84e8a618fac3c70053cf1d0f50
John Cravener <johncrav@microsoft.com>
2022-07-14 17:38:36 -0700

    Continuing to flesh out problem: Remove Duplicates from Sorted List

--- LeetCode/Remove Duplicates from Sorted List/Program.cs
+++ LeetCode/Remove Duplicates from Sorted List/Program.cs
@@ -1,29 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] lst = new int[] { 1, 2, 3, 4, 5};
+int[] lst = new int[] { 1, 2, 2, 3, 4, 5, 5, 5, 5};
 
-ListNode head = new ListNode();
+LinkedList linkedList = new LinkedList();
 
 foreach(var i in lst)
 {
-    head.Val = i;
-    var newNode = new ListNode();
-    newNode.Next = head;
-    head = newNode;
+    linkedList.Append(i);
 }
 
-var currNode = head;
+LinkedList returnList = new LinkedList();
+HashSet<int> set = new HashSet<int>();
+
+var currNode = linkedList.Head;
 while(currNode.Next != null)
 {
-    Console.WriteLine(currNode.Val);
     currNode = currNode.Next;
+
+    if (!set.Contains(currNode.Val))
+    {
+        set.Add(currNode.Val);
+
+        returnList.Append(currNode.Val);
+    }
 }
 
-// Definition for singly-linked list.
+currNode = returnList.Head;
+while(currNode.Next != null)
+{
+    currNode = currNode.Next;
+    Console.WriteLine(currNode.Val);
+}
+
+// Definition for singly-linked list node.
 public class ListNode
 {
-    public int Val { get; set; } = 500;
+    public int Val { get; set; }
     public ListNode? Next { get; set; } = null;
+}
+
+public class LinkedList
+{
+    public ListNode Head { get; set; }
+    
+    public LinkedList()
+    {
+        Head = new ListNode();
+    }
+
+    public void Append(int data)
+    {
+        var newNode = new ListNode
+        { 
+            Val = data
+        };
+        var curr = Head;
+        while(curr.Next != null)
+        {
+            curr = curr.Next;
+        }
+        curr.Next = newNode;
+    }
 }
 public class Solution
 {
