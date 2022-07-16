d24b81f7f58d701caf9129d8bbf266735bb185f5
john cravener <jcravener@hotmail.com>
2022-07-16 12:03:31 -0700

    Continuing to fles out problem: Remove Duplicates from Sorted List

--- LeetCode/Remove Duplicates from Sorted List/Program.cs
+++ LeetCode/Remove Duplicates from Sorted List/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] lst = new int[] { 1, 2, 2, 3, 4, 5, 5, 5, 5};
+int[] lst = new int[] { 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7};
 
 LinkedList linkedList = new LinkedList();
 
@@ -25,12 +25,52 @@ while(currNode.Next != null)
     }
 }
 
+currNode = linkedList.Head;
+while (currNode.Next != null)
+{
+    currNode = currNode.Next;
+    Console.WriteLine(currNode.Val);
+}
+
+Console.WriteLine("------");
+
 currNode = returnList.Head;
 while(currNode.Next != null)
 {
     currNode = currNode.Next;
     Console.WriteLine(currNode.Val);
 }
+
+Console.WriteLine("------");
+
+currNode = linkedList.Head;
+var savedNode = new ListNode();
+set.Clear();
+while (currNode.Next != null)
+{
+    currNode = currNode.Next;
+
+    if (!set.Contains(currNode.Val)) // We have not seen this one yet
+    {
+        set.Add(currNode.Val);
+        savedNode = currNode;
+    }
+
+    // we have seen this one, so check the other one
+
+    // As soon as we see a new one, point the save one to the new one
+}
+
+Console.WriteLine("------");
+
+
+currNode = linkedList.Head;
+while (currNode.Next != null)
+{
+    currNode = currNode.Next;
+    Console.WriteLine(currNode.Val);
+}
+
 
 // Definition for singly-linked list node.
 public class ListNode
@@ -42,10 +82,12 @@ public class ListNode
 public class LinkedList
 {
     public ListNode Head { get; set; }
+    public ListNode Tail { get; set; }
     
     public LinkedList()
     {
         Head = new ListNode();
+        Tail = Head;
     }
 
     public void Append(int data)
@@ -54,12 +96,8 @@ public class LinkedList
         { 
             Val = data
         };
-        var curr = Head;
-        while(curr.Next != null)
-        {
-            curr = curr.Next;
-        }
-        curr.Next = newNode;
+        Tail.Next = newNode;
+        Tail = newNode;
     }
 }
 public class Solution
