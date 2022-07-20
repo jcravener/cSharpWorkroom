d89f762d4ac89e0edc63396a073468f85dd34462
john cravener <jcravener@hotmail.com>
2022-07-19 19:35:33 -0700

    Fleshing out: 88. Merge Sorted Array

---
+++ LeetCode/Merge Sorted Array/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+int[] nums2 = { 2, 5, 6 };
+
+int max = nums1.Length;
+
+for(int i = nums1.Length - 1; i >= nums2.Length; i--)
+{    
+    nums1[i] = nums2[i - nums2.Length];
+}
+
+Array.Sort(nums1);
+
+for(int i = 0; i < max; i++)
+{
+    Console.WriteLine(nums1[i]);
+}
