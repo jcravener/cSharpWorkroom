8a539ebac28ef7ccabba3ffc6cd278fd58017c06
john cravener <jcravener@hotmail.com>
2022-07-19 21:52:24 -0700

    Fleshing out problem: 94. Binary Tree Inorder Traversal

---
+++ LeetCode/Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+TreeNode root = new TreeNode(1, null, null);
+TreeNode two = new TreeNode(2, null, null);
+TreeNode three = new TreeNode(3, null, null);
+
+root.Right = two;
+two.Left = three;
+
+Solution solution = new Solution();
+
+foreach(int i in solution.InorderTraversal(root))
+{
+    Console.WriteLine(i);
+}
+
+
+// Below is solution one from Leetcode:
+
+public class TreeNode
+{
+    public int Val { get; set; }
+    public TreeNode? Left { get; set; }
+    public TreeNode? Right { get; set; }
+    public TreeNode(int val, TreeNode? left, TreeNode? right)
+    {
+        Val = val;
+        Left = left;
+        Right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode? root)
+    {
+        List<int> res = new List<int>();
+        Helper(root, res);
+        return res;
+    }
+
+    public void Helper(TreeNode? root, IList<int> res)
+    {
+        if(root != null)
+        {
+            Helper(root.Left, res);
+            res.Add(root.Val);
+            Helper(root.Right, res);
+        }
+    }
+}
