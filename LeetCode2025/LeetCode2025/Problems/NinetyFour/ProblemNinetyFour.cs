f1ccc2ddd0b68b433e281192a8ea8622ce052151
John Cravener <johncrav@microsoft.com>
2025-04-08 17:21:19 -0700

    Fleshing out problem 94

---
+++ LeetCode2025/LeetCode2025/Problems/NinetyFour/ProblemNinetyFour.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode2025.Problems.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode2025.Problems.NinetyFour
+{
+    public class ProblemNinetyFour : ProblemBase
+    {
+        public TreeNode Node { get; set; }
+        
+        public void RunProblem(TreeNode node)
+        {
+            Node = node;
+        }
+
+
+        private void Solve()
+        {
+            List<int> list = new();
+
+            Traverse(Node, list);
+
+            int[] ints = new int[list.Count];
+
+            foreach (int i in list) ints[i] = i;
+
+            Console.WriteLine(ToString(ints));
+        }
+
+
+        private void Traverse(TreeNode? node, List<int> list)
+        {
+            if(node != null)
+            {
+                Traverse(node.Left, list);
+                list.Add(node.Value);
+                Traverse(node.Right, list);
+            }
+        }
+    }
+}
---
+++ LeetCode2025/LeetCode2025/Problems/Util/TreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode2025.Problems.Util
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+
+        public TreeNode? Left { get; set; }
+
+        public TreeNode? Right { get; set; }
+
+        public TreeNode(int value, TreeNode? left, TreeNode? right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+}
