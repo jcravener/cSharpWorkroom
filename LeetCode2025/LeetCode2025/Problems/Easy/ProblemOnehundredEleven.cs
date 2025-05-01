eba9231198287a96dfce34bf916d9e6a63b778b6
John Cravener <johncrav@microsoft.com>
2025-04-30 17:02:39 -0700

    Starting on problem 111

---
+++ LeetCode2025/LeetCode2025/Problems/Easy/ProblemOnehundredEleven.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode2025.Problems.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode2025.Problems.Easy
+{
+    public class ProblemOnehundredEleven : ProblemBase
+    {
+        public TreeNode Root { get; set; }
+        
+        public ProblemOnehundredEleven(int example)
+        {
+            switch (example)
+            {
+                case 1:
+                    Root = new TreeNode(3);
+                    Root.Left = new TreeNode(9);
+                    Root.Right = new TreeNode(20);
+                    Root.Right.Left = new TreeNode(15);
+                    Root.Right.Right = new TreeNode(7);
+                    break;
+                case 2:
+                    Root = new TreeNode(2);
+                    Root.Right = new TreeNode(3);
+                    break;
+                default:
+                    throw new Exception($"Example {example} unknown.");
+            }
+        }
+    }
+}
