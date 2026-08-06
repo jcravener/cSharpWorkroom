c0b8078d7e64a2f0f01393b80db5174d5fc33339
John Cravener <johncrav@microsoft.com>
2026-08-05 19:56:15 -0700

    Flesing out BST erxample.

---
+++ CodingPractice2026/CodingPractice2026/Examples/ExampleBST.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace CodingPractice2026.Examples
+{
+    internal class ExampleBST : ExampleBase
+    {
+
+        private int[] Random {get; set;}
+        
+        public ExampleBST(int[] random) 
+        {
+            Random = randome;
+        }
+
+        public void RunProblem()
+        {
+            TreeNode root = null;
+
+            foreach(int val in Random)
+            {
+                root = Insert(root, val);
+            }
+        }
+
+        public TreeNode Insert(TreeNode root, int value){
+
+            if(root == null)
+                return new TreeNode(value)
+
+            if(value < root.Value)
+                root.Left = Insert(root.Left, value)
+            else
+                root.Right = Insert(root.Right, value)
+
+            return root;
+        }
+   }
+}
