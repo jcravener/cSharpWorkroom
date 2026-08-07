433cff7686628fe5466a8552be74d1d99910c5d5
John Cravener <johncrav@microsoft.com>
2026-08-06 19:40:42 -0700

    Fleshing out DFS traversal methods

--- CodingPractice2026/CodingPractice2026/Examples/ExampleBST.cs
+++ CodingPractice2026/CodingPractice2026/Examples/ExampleBST.cs
@@ -7,6 +7,8 @@ namespace CodingPractice2026.Examples
     {
 
         private int[] Random {get; set;}
+
+        private List<int> List  = new();
         
         public ExampleBST(int[] random) 
         {
@@ -15,15 +17,80 @@ namespace CodingPractice2026.Examples
 
         public void RunProblem()
         {
+            TreeNode? root = BuildTree();
+            
+            InOrderDFS(root);
+
+            Console.WriteLine($"In-Order DFS:\n{string.Join(" ", List)}");
+
+            List.Clear();
+
+            PreOrderDFS(root);
+
+            Console.WriteLine($"Pre-order DFS:\n{string.Join(" ", List)}");
+
+            List.Clear();
+
+            PostOrderDFS(root);
+
+            Console.WriteLine($"Post-order DFS:\n{string.Join(" ", List)}");
+        }
+
+        private void InOrderDFS(TreeNode? root)
+        {
+            if(root == null)
+                return;
+
+            if(root.Left != null)
+                InOrderDFS(root.Left);
+
+            List.Add(root.Value);
+
+            if(root.Right != null)
+                InOrderDFS(root.Right);
+        }
+
+        private void PreOrderDFS(TreeNode? root)
+        {
+            if(root == null)
+                return;
+            
+            List.Add(root.Value);
+
+            if(root.Left != null)
+                PreOrderDFS(root.Left);
+
+            if(root.Right != null)
+                PreOrderDFS(root.Right);
+        }
+
+        private void PostOrderDFS(TreeNode? root)
+        {
+            if(root == null)
+                return;
+
+            if(root.Left != null)
+                PostOrderDFS(root.Left);
+            
+            if(root.Right != null)
+                PostOrderDFS(root.Right);
+
+            List.Add(root.Value);
+        }
+
+        public TreeNode? BuildTree()
+        {
             TreeNode? root = null;
 
             foreach(int val in Random)
             {
                 root = Insert(root, val);
             }
+
+            return root;            
         }
 
-        public TreeNode Insert(TreeNode? root, int value){
+        private TreeNode Insert(TreeNode? root, int value){
 
             if(root == null)
                 return new TreeNode(value);
